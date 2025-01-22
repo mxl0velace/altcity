bea9c9183e63d4c2ea5e690e4c6a8ec0545919d3
Harper Barnett <harper@barnett.work>
2025-01-22 20:47:51 +0000

    Initial collection structure and page rel- #3

---
+++ src/routes/collection/+page.server.ts
@@ -0,0 +1,22 @@
+import { error, redirect } from '@sveltejs/kit';
+
+export const load = async ({locals, params}) => {
+    if (!locals.user){
+        redirect(303,"/login");
+    }
+    const getUserWithCollection = async() => {
+        try {
+            const userArts = await locals.pb.collection('users').getOne(locals?.user?.id || "", {
+                expand: "cardcollection.artist"
+            })
+            return userArts;
+        } catch (err: any) {
+            console.log(err);
+            //throw error(err.status, err.message)
+        }
+    }
+    return {
+        fulluser: await getUserWithCollection(),
+        title: "Alt Arts"
+    }
+}
