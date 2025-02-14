1cc06e00ba8dae0c5d258b2c3d8053d22fcd3c44
Harper Barnett <harper@barnett.work>
2025-02-14 22:59:45 +0000

    Add auto-watermarking Closes #29

--- src/routes/artist/[id]/art/new/+page.server.ts
+++ src/routes/artist/[id]/art/new/+page.server.ts
@@ -1,10 +1,33 @@
 import { isRedirect, redirect } from "@sveltejs/kit";
+import sharp from "sharp";
 
 export const actions = {
     default: async ({request, locals, params}) => {
         const data = await request.formData();
-
-        data.append("artist", params.id)
+        if (data.get("watermark")) {
+            const img = data.get("image");
+            //@ts-ignore
+            const simg = sharp(await img.arrayBuffer());
+            const meta = await simg.metadata();
+            const svgWatermark = `
+                <svg width="${meta.width}" height="${meta.height}">
+                    <style>
+                        .title { fill: #001; font-size: ${(meta?.width || 500)/6}px; font-weight: bold; opacity: 10%;}
+                    </style>
+                    <text x="50%" y="50%" text-anchor="middle" class="title">alt city grid</text>
+                    <text x="50%" y="25%" text-anchor="middle" class="title">alt city grid</text>
+                    <text x="50%" y="75%" text-anchor="middle" class="title">alt city grid</text>
+                </svg>        `;
+            //@ts-ignore
+            const svgBuffer = Buffer.from(svgWatermark);
+            const watermarkedImage = await simg.composite([{
+                input: svgBuffer,
+                top: 0,
+                left: 0,
+            }]).toBuffer();
+            data.set("image", new Blob([watermarkedImage]));
+        }
+        data.append("artist", params.id);
 
         try {
             var result = await locals.pb.collection('art').create(data);
