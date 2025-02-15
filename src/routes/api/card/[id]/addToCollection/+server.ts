816a050a12e560176695b5ab29be9289ed11adae
Harper Barnett <harper@barnett.work>
2025-02-15 01:22:25 +0000

    Create collection if collection does not exist

--- src/routes/api/card/[id]/addToCollection/+server.ts
+++ src/routes/api/card/[id]/addToCollection/+server.ts
@@ -9,9 +9,22 @@ export const POST: RequestHandler = async ({request, locals, params}) => {
     if (locals.user.id != userId) {
         throw error (403);
     }
-    const cardCollection = await locals.pb.collection("cardcollection").getFirstListItem(`owner.id = "${userId}"`)
-    await locals.pb.collection("cardcollection").update(cardCollection.id,{
-        'cards+': params.id 
-    })
+    var cardCollection;
+    try {
+        cardCollection = await locals.pb.collection("cardcollection").getFirstListItem(`owner.id = "${userId}"`)
+    }
+    catch (error) {
+        cardCollection = null
+    }
+    if (cardCollection == null) {
+        await locals.pb.collection("cardcollection").create({
+            owner: userId,
+            cards: [params.id]
+        })
+    } else {
+        await locals.pb.collection("cardcollection").update(cardCollection.id,{
+            'cards+': params.id 
+        })    
+    }
     return new Response();
 };
