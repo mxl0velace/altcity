4d8c701b9f28fb9b7e4faec7aad5f3d9ba7f7437
Harper Barnett <harper@barnett.work>
2025-03-06 18:14:22 +0000

    Allow editing artist page Closes #62

--- src/routes/artist/[id]/+page.server.ts
+++ src/routes/artist/[id]/+page.server.ts
@@ -1,4 +1,4 @@
-import { error } from '@sveltejs/kit';
+import { error, isRedirect, redirect } from '@sveltejs/kit';
 
 export const load = async ({locals, params}) => {
     const getArtist = async(artistId: any) => {
@@ -18,4 +18,28 @@ export const load = async ({locals, params}) => {
     /*return {
         artist: getArtist(params.id),
     }*/
+}
+
+export const actions = {
+    default: async ({request, locals, params}) => {
+        const data = await request.formData();
+        try {
+            var toSend = {
+                name: data.get("name"),
+                link: data.get("link"),
+            }
+            if (data.get("logo")){
+                if (!data.get("logo").size == 0) {
+                    toSend.logo = data.get("logo");
+                }
+            }    
+            let result = await locals.pb.collection('artist').update(params.id, toSend);
+            redirect(303, "/artist/"+result.id);
+        } catch (error) {
+            if (isRedirect(error)){
+                throw error;
+            }
+            console.log(error);
+        }
+    }
 }
